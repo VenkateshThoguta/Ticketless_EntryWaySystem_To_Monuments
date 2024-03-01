75e4da0fcb9924e179d02b9986d676379d8f445b
VenkateshThoguta <123985806+VenkateshThoguta@users.noreply.github.com>
2024-03-02 01:09:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ app.use(express.json());
 
 const monuRoute=require('./routes/monuRoute')
 
-app.use(cors({
-  origin:["https://ticketless-entry-way-system-to-monuments.vercel.app/"],
-  methods:["POST","GET"]
-}))
+
 
 app.use('/api/monuments',monuRoute)
 app.use('/api/users',userRoute)
