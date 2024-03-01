2ee51e2ba0e448175fb3c9200c63b8c310f33751
VenkateshThoguta <123985806+VenkateshThoguta@users.noreply.github.com>
2024-03-02 00:23:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ const monuRoute=require('./routes/monuRoute')
 app.use('/api/monuments',monuRoute)
 app.use('/api/users',userRoute)
 
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'/client/build')))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-    })
-}
-else{
-    app.get('/',(req,res)=>{
-        res.send(  'server working')
-    })
-}
+// if(process.env.NODE_ENV==='production'){
+//     app.use(express.static(path.join(__dirname,'/client/build')))
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+//     })
+// }
+// else{
+//     app.get('/',(req,res)=>{
+//         res.send(  'server working')
+//     })
+// }
 
 
 
